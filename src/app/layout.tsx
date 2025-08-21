69b54fb0622172d7b55113e994e9ec2d9c9b9a70
刘树 <shuliu298@gmail.com>
2025-08-21 05:36:09 +0000

    可以，配置之后教我如何安装apk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,8 @@ export default function RootLayout({
   return (
     <html lang="zh-CN" suppressHydrationWarning>
       <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
